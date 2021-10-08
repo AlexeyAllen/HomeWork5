0b80a4951b04878edc0a66b04eab8148e3a4402a
Aleksei Allen <aqml@mail.ru>
2021-10-08 19:31:46 +0300

    added 1 task

---
+++ MinMax/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[15];
+            Random random = new Random();
+            int max = 0;
+            int min = 50;
+
+            for (int i = 0; i < 15; i++)
+            {
+                array[i] = random.Next(0, 50);
+                Console.Write("{0} ", array[i]);
+
+                if (array[i] > max) max = array[i];
+                if (array[i] < min) min = array[i];
+            }
+
+            Console.WriteLine("max= {0}; min = {1}", max, min);
+            Console.ReadKey();
+        }
+    }
+}
